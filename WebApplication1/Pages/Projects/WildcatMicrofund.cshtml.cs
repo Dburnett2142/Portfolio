fa60dd20cc73e98a891808f35a7a195c0a7e965f
Dburnett2142 <davidburnett2@mail.weber.edu>
2024-04-01 21:35:24 -0600

    Basic site layout

---
+++ WebApplication1/Pages/Projects/WildcatMicrofund.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages.Projects
+{
+    public class WildcatMicrofundModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
